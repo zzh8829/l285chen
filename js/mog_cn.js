8c4cc05a4ebf0bcf26362b6cbf52ba135a2b6ba2
Letian Chen <l285chen@uwaterloo.ca>
2016-02-20 01:49:23 -0500

    fix header

--- js/mog_cn.js
+++ js/mog_cn.js
@@ -1,3 +1,10 @@
 $("#mog_now").click(function(){
-	alert("恭喜你！G神的光芒照耀着你！")	
+	var r = confirm("您确定要膜G吗？");
+	if (r == true) {
+		alert("恭喜你！G神的光芒照耀着你！");
+	}
+	else {
+		$("#mog_now").hide();
+		$("#mog_now").parent().find("img").show();
+	}
 })
