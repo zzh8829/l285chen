521b33b88196e49998bf83b612c637ff877c6a9d
Letian Chen <l285chen@uwaterloo.ca>
2016-02-19 19:08:22 -0500

    not completed

--- js/main.js
+++ js/main.js
@@ -4,3 +4,8 @@ $("#aspiring_intro").hover(function(){
 function(){
 	$("#god_quote").hide();
 });
+
+$("#reason_to_live").click(function(){
+	$("#mog_img").find('span > img').fadeIn();
+	$("#reason_to_live").replaceWith('<img src="img/mog1.jpg">');
+})
